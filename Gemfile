7e0b36609f952407099a6110ef7d4fead4b83702
Michal Stanke <michal@stanke.cz>
2018-12-12 13:02:43 +0100

    Stažení RSS feedů a vyrenderování do HTML ze `site.data` (#8)
    * Stažení RSS feedů a vyrenderování ze `site.data`
    * ENCZTranslator pro překlad nadpisů z feedu v HTML
    * Zrušeno nepotřebné stahování souborů do `remote_content`

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 gem 'jekyll', '~> 3.8', '>= 3.8.5'
 gem 'jekyll-redirect-from', '~> 0.14.0'
 gem 'open_uri_redirections', '~> 0.2.1'
+gem 'json', '~> 2.1'
+gem 'feedparser', '~> 2.1', '>= 2.1.1'
 
 group :test do
   gem 'html-proofer', '~> 3.9', '>= 3.9.2'
--- _plugins/Jekyll_Get_Remote_Content.rb
+++ _plugins/Jekyll_Get_Remote_Content.rb
@@ -1,9 +1,11 @@
+require 'json'
 require 'open-uri'
 require 'open_uri_redirections'
+require 'feedparser'
 
 module Jekyll_Get_Remote_Content
 
-  class Fetcher
+  class FeedFetcher
     def self.fetchFor(site)
       config = site.config['remote_content']
       if !config
@@ -12,19 +14,18 @@ module Jekyll_Get_Remote_Content
       if !config.kind_of?(Array)
         config = [config]
       end
-      Dir.mkdir('remote_content') unless Dir.exist?('remote_content')
+      site.data['rss_feeds'] = []
       config.each do |remote|
         open(remote['url'], 'r', :allow_redirections => :all) do |remote_content|
-          open('remote_content/'+remote['name'], 'wb') do |local_file|
-            local_file.write(remote_content.read)
-          end
+          site.data['rss_feeds'].push(remote['name'])
+          site.data[remote['name']] = JSON.parse(FeedParser::Parser.parse(remote_content.read).to_json)
         end
       end
     end
   end
 
-  Jekyll::Hooks.register :site, :after_reset do |site|
-    Fetcher.fetchFor(site)
+  Jekyll::Hooks.register :site, :post_read do |site|
+    FeedFetcher.fetchFor(site)
   end
 
 end
